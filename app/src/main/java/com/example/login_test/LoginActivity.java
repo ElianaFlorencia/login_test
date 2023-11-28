11c06b1b30e90a706a8b4f18b8e2b328c5b983da
Eliana <eliana.cruz@davinci.edu.ar>
2023-11-28 18:44:55 -0300

    feat: login with user and password

--- app/src/main/java/com/example/login_test/LoginActivity.java
+++ app/src/main/java/com/example/login_test/LoginActivity.java
@@ -1,17 +1,47 @@
 package com.example.login_test;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+
     public void login(String email, String password) {
-        Log.i( "firebase", "email:" + email);
-        Log.i(  "firebase",  "password: "+ password);
+        Log.i("firebase", "email:" + email);
+        Log.i("firebase", "password: " + password);
+
+        // lo que hace esto es: si apretas el login, va a la main pero cuando llega como no tiene el
+        //usuario va devuelta al login
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+
+                        } else {
+                            Toast.makeText(LoginActivity.this, " Falló el login ",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
     }
 
 
@@ -33,5 +63,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
     }
 }
