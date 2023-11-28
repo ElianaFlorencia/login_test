a4af0600ad0769ff93c9987dda051f3c371fbffd
Eliana <eliana.cruz@davinci.edu.ar>
2023-11-28 16:40:24 -0300

    feat: add login setup

--- app/src/main/java/com/example/login_test/MainActivity.java
+++ app/src/main/java/com/example/login_test/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.login_test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Log.i ("firebase", "hay usuario");
+
+        } else{
+            Log.i ("firebase", "deberia logearme porque no hay usuario");
+
+        }
+    }
+
+
+
 }
